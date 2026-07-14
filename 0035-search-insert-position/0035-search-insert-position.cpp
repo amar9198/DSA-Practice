4f60ad3cc7d00d34cb332f87d56b9c23d3f329d8
Amar Prajapati <amarprajapati42982@gmail.com>
2026-07-14 08:55:24 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.24%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,16 +3,16 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int n=nums.size();
         int s=0,e=n-1;
+        int ans=n;
         while(s <= e){
             int mid=(s+e)/2;
-            if(target==nums[mid]){
-                return mid;
-            }else if(target<nums[mid]){
+            if(nums[mid]>=target){
+                ans=mid;
                 e=mid-1;
             }else{
                 s=mid+1;
             }
         }
-        return e+1;
+        return ans;  
     }
 };
