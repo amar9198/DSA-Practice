362ed953923b1d289276b737594117b45b2fb90c
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-03 08:52:54 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,20 @@
+#include <unordered_map>
+#include <string>
+using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-         if (s.length() != t.length()) {
-        return false;  // Strings can't be anagrams if lengths are different
-    }
+         unordered_map<char,int>ma;
+         unordered_map<char,int>mb;
 
-    // Sort both strings
-    sort(s.begin(), s.end());
-    sort(t.begin(), t.end());
-
-    // Case 2: check if every character of str1 and str2 matches with each other
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] != t[i]) {
-            return false;  // If any character doesn't match, they aren't anagrams
+        for(const auto &el:s){
+            ma[el]++;
         }
+           for(const auto &el:t){
+            mb[el]++;
+        }
+        return ma==mb;
     }
 
-    return true;  // Strings are anagrams
-    }  
+
 };
