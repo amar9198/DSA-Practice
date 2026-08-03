a98058ab8375ce03be5d9018eb4b0a1d144339cd
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-03 09:01:39 +0530

    Time: 0 ms (100%), Space: 9.8 MB (50.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,26 @@
-#include <unordered_map>
-#include <string>
-using namespace std;
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-         unordered_map<char,int>ma;
-         unordered_map<char,int>mb;
+    if (s.length() != t.length()) 
+        return false;
 
-        for(const auto &el:s){
-            ma[el]++;
-        }
-           for(const auto &el:t){
-            mb[el]++;
-        }
-        return ma==mb;
+    int freq[26] = {0};
+
+    for (int i = 0; i < s.length(); i++) {
+       freq[s[i] - 'a']++;
     }
 
+    for (int i = 0; i < t.length(); i++) {
+        freq[t[i] - 'a']--;  // Decrement frequency for each character in str2
+    }
 
+    // Check if all frequencies are zero, meaning both strings have the same characters
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0)  // If any frequency is non-zero, they are not anagrams
+            return false;
+    }
+
+    return true;  // The strings are anagrams
+}
 };
