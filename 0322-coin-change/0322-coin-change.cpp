adc3a501ace0e900675470621fec25011f984f0a
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-03 18:16:35 +0530

    Time: 38 ms (49.74%), Space: 19.2 MB (54.72%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solveRec(vector<int>&num,int x,vector<int>&dp){
+        if(x==0) return 0;
+        if(x<0) return INT_MAX;
+        if(dp[x]!=-1) return dp[x];
+
+        int mini=INT_MAX;
+        for(int i=0;i<num.size();i++){
+            int ans=solveRec(num,x-num[i],dp);
+            if(ans!=INT_MAX)
+                mini=min(mini,1+ans);
+        }
+        dp[x]=mini;
+        return mini;
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1,-1);
+        int ans=solveRec(coins,amount,dp);
+        if(ans==INT_MAX) return -1;
+        return ans;
+        
+    }
+};
