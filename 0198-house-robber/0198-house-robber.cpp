8950986457f7114670b1ae2a886ec1888846964e
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-05 17:16:09 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-int SolveMem(vector<int>&nums,int n,vector<int>&dp){
-    if(n<0) return 0;
-    if(n==0) return nums[0];
-    if(dp[n]!=-1) return dp[n];
-    int incl=SolveMem(nums,n-2,dp)+nums[n];
-    int excl=SolveMem(nums,n-1,dp)+0;
-    dp[n]=max(incl,excl);
-    return dp[n];
-    
-}
-  int rob(vector<int>& nums) {
-  int n=nums.size();
-    vector<int>dp(n,-1);
-    return SolveMem(nums,n-1,dp);
-  }
+    int SolveTab(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int incl = nums[i];
+            if (i > 1)
+                incl += dp[i - 2];
+
+            int excl = dp[i - 1];
+
+            dp[i] = max(incl, excl);
+        }
+
+        return dp[n - 1];
+    }
+
+    int rob(vector<int>& nums) {
+        return SolveTab(nums);
+    }
 };
-
-
