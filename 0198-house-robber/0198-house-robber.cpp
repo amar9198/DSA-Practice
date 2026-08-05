b09b5951b0fe32c56a0c0c07b3774422142f6fa0
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-05 17:12:30 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int SolveSpace(vector<int>&nums){
-    int n=nums.size();
-    int prev2=0;
-    int prev1=nums[0];
-    for(int i=1;i<n;i++){
-        int incl=prev2+nums[i];
-        int excl=prev1+0;
-        int ans=max(incl,excl);
-        prev2=prev1;
-        prev1=ans;
-    }
-    return prev1;
+int SolveMem(vector<int>&nums,int n,vector<int>&dp){
+    if(n<0) return 0;
+    if(n==0) return nums[0];
+    if(dp[n]!=-1) return dp[n];
+    int incl=SolveMem(nums,n-2,dp)+nums[n];
+    int excl=SolveMem(nums,n-1,dp)+0;
+    dp[n]=max(incl,excl);
+    return dp[n];
+    
 }
-    int rob(vector<int>& nums) {
-        return SolveSpace(nums);
-        
-    }
+  int rob(vector<int>& nums) {
+  int n=nums.size();
+    vector<int>dp(n,-1);
+    return SolveMem(nums,n-1,dp);
+  }
 };
+
+
