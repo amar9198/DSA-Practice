88a4ed1b9b423875d2de686dd01d18ca7ffc59dd
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-18 04:04:44 +0530

    Time: 0 ms (100%), Space: 13.1 MB (83.26%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int solveTab(int n,vector<int>&days,vector<int>&cost){
-    vector<int>dp(n+1,INT_MAX);
-    dp[n]=0;
-    for(int k=n-1;k>=0;k--){
-        int option1=cost[0]+dp[k+1];
-        int i;
-        for(i=k;i<n && days[i] < days[k]+7;i++);
-        int option2=cost[1]+dp[i];
-
-        for(i=k;i<n && days[i]<days[k]+30;i++);
-        int option3=cost[2]+dp[i];
-        dp[k]=min(option1,min(option2,option3));
-    }
-    return dp[0];
-}
     int mincostTickets(vector<int>& days, vector<int>& costs) {
     int n = days.size();
-    return solveTab(n,days,costs);
-        
+    int ans=0;
+    queue<pair<int,int>>month;
+    queue<pair<int,int>>week;
+    for(int day:days){
+        while(!month.empty() && month.front().first+30<=day){
+            month.pop();
+        }
+        while(!week.empty() && week.front().first+7<=day){
+            week.pop();
+        }
+        week.push(make_pair(day,ans+costs[1]));
+        month.push(make_pair(day,ans+costs[2]));
+        ans=min(ans+costs[0],min(week.front().second,month.front().second));
     }
+    return ans;
+
+}
 };
