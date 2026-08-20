afd846dc42ee35fc064c9ea8aa23db8760c419f5
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-20 23:29:54 +0530

    Time: 35 ms (90.15%), Space: 233.9 MB (74.73%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
 
-    int SolveMem(vector<int>& obstacles,int currlane,int currpos,vector<vector<int>>&dp){
+    int SolveTab(vector<int>& obstacles){
         int n=obstacles.size()-1;
-        if(currpos==n) return 0;
-        if(dp[currlane][currpos]!=-1) return dp[currlane][currpos];
-        if(obstacles[currpos+1]!=currlane){
-            return SolveMem(obstacles,currlane,currpos+1,dp);
-        }else{
-            int ans=INT_MAX;
-            for(int i=1;i<=3;i++){
-                if(currlane!=i && obstacles[currpos]!=i)
-                    ans = min(ans,1+SolveMem(obstacles,i,currpos,dp));
+        vector<vector<int>>dp(4,vector<int>(obstacles.size(),1e9));
+        dp[0][n]=0;
+        dp[1][n]=0;
+        dp[2][n]=0;
+        dp[3][n]=0;
+        for(int currpos=n-1;currpos>=0;currpos--){
+            for(int currlane=1;currlane<=3;currlane++){
+                if(obstacles[currpos+1]!=currlane){
+                    dp[currlane][currpos]= dp[currlane][currpos+1];
+                }else{
+                    int ans=1e9;
+                    for(int i=1;i<=3;i++){
+                       if(currlane!=i && obstacles[currpos]!=i)
+                          ans = min(ans,1+dp[i][currpos+1]);
+                    }
+                    dp[currlane][currpos]=ans;
+                }
             }
-            dp[currlane][currpos]=ans;
-            return dp[currlane][currpos];
         }
+        return min(dp[2][0],min(1+dp[1][0],1+dp[3][0]));
     }
     int minSideJumps(vector<int>& obstacles){
-        vector<vector<int>>dp(4,vector<int>(obstacles.size(),-1));
-        return SolveMem(obstacles,2,0,dp);
+        return SolveTab(obstacles);
         
     }
 };
