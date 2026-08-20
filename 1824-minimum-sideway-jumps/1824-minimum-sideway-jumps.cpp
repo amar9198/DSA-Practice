6830b02a98b666d466c8e3f549151324adabd67d
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-20 21:22:07 +0530

    Time: 56 ms (70.34%), Space: 276.2 MB (52.14%) - LeetHub

---
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    int SolveMem(vector<int>& obstacles,int currlane,int currpos,vector<vector<int>>&dp){
+        int n=obstacles.size()-1;
+        if(currpos==n) return 0;
+        if(dp[currlane][currpos]!=-1) return dp[currlane][currpos];
+        if(obstacles[currpos+1]!=currlane){
+            return SolveMem(obstacles,currlane,currpos+1,dp);
+        }else{
+            int ans=INT_MAX;
+            for(int i=1;i<=3;i++){
+                if(currlane!=i && obstacles[currpos]!=i)
+                    ans = min(ans,1+SolveMem(obstacles,i,currpos,dp));
+            }
+            dp[currlane][currpos]=ans;
+            return dp[currlane][currpos];
+        }
+    }
+    int minSideJumps(vector<int>& obstacles){
+        vector<vector<int>>dp(4,vector<int>(obstacles.size(),-1));
+        return SolveMem(obstacles,2,0,dp);
+        
+    }
+};
