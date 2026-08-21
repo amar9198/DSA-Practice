387844e0a254bb293c6740ea114b7d3bb5c73cd1
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-21 23:02:02 +0530

    Time: 7 ms (48.24%), Space: 12.5 MB (50.18%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
 
-    int SolveTab(vector<int>& satisfaction){
+    int SolveSpace(vector<int>& satisfaction){
         int n=satisfaction.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>curr(n+1,0);
+        vector<int>next(n+1,0);
+
         for(int index=n-1;index>=0;index--){
             for(int time=index;time>=0;time--){
-                int include=satisfaction[index]*(time+1)+dp[index+1][time+1];
-                int exclude=0+dp[index+1][time];
-                dp[index][time]=max(include,exclude);
+                int include=satisfaction[index]*(time+1)+next[time+1];
+                int exclude=0+next[time];
+                curr[time]=max(include,exclude);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
-        
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
-        return SolveTab(satisfaction);
-        
+        return SolveSpace(satisfaction);   
     }
 };
