573358bf5b3e7fdb3e21551ee453465d5ffb1713
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-21 22:48:43 +0530

    Time: 14 ms (38.02%), Space: 24.9 MB (34.83%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
 
-    int SolveMem(vector<int>& satisfaction,int index,int time,vector<vector<int>>&dp){
-        if(index==satisfaction.size()) return 0;
-        if(dp[index][time]!=-1) return dp[index][time];
-        int include=satisfaction[index]*(time+1)+SolveMem(satisfaction,index+1,time+1,dp);
-        int exclude=0+SolveMem(satisfaction,index+1,time,dp);
-        return dp[index][time]=max(include,exclude);
+    int SolveTab(vector<int>& satisfaction){
+        int n=satisfaction.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int index=n-1;index>=0;index--){
+            for(int time=index;time>=0;time--){
+                int include=satisfaction[index]*(time+1)+dp[index+1][time+1];
+                int exclude=0+dp[index+1][time];
+                dp[index][time]=max(include,exclude);
+            }
+        }
+        return dp[0][0];
     }
+        
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
-        int n=satisfaction.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return SolveMem(satisfaction,0,0,dp);
+        return SolveTab(satisfaction);
         
     }
 };
