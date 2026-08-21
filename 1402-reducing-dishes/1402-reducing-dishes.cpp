2b9ff66b2473fcfd35d6837ebc8d3c27d5ba83d5
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-21 21:55:35 +0530

    Time: 31 ms (8.78%), Space: 25 MB (15.06%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int SolveMem(vector<int>& satisfaction,int index,int time,vector<vector<int>>&dp){
+        if(index==satisfaction.size()) return 0;
+        if(dp[index][time]!=-1) return dp[index][time];
+        int include=satisfaction[index]*(time+1)+SolveMem(satisfaction,index+1,time+1,dp);
+        int exclude=0+SolveMem(satisfaction,index+1,time,dp);
+        return dp[index][time]=max(include,exclude);
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(),satisfaction.end());
+        int n=satisfaction.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return SolveMem(satisfaction,0,0,dp);
+        
+    }
+};
