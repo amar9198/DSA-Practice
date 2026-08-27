2f32d5ca8f1c8c9a04a309aa92bed8e0f00eeb15
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-27 19:12:27 +0530

    Time: 122 ms (5.75%), Space: 138.7 MB (7.14%) - LeetHub

---
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int SolveMem(vector<int>& nums1, vector<int>& nums2,int index,bool swapped,vector<vector<int>>&dp){
+        if(index==nums1.size()) return 0;
+        if(dp[index][swapped]!=-1) return dp[index][swapped];
+        int ans=INT_MAX;
+        int prev1=nums1[index-1];
+        int prev2=nums2[index-1];
+        if(swapped) swap(prev1,prev2);
+        if(nums1[index]>prev1 && nums2[index]>prev2)
+            ans=SolveMem(nums1,nums2,index+1,0,dp);
+        if(nums1[index]>prev2 && nums2[index]>prev1)
+            ans=min(ans,1+SolveMem(nums1,nums2,index+1,1,dp));
+        return dp[index][swapped]=ans;        
+    }
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.begin(),-1);
+        nums2.insert(nums2.begin(),-1);
+        bool swapped=0;
+        int n=nums1.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return SolveMem(nums1,nums2,1,swapped,dp);
+        
+    }
+};
