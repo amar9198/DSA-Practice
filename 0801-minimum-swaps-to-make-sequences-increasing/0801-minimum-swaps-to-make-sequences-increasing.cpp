a37cf961d69dd672a997e0429dde61895b52ef6d
Amar Prajapati <amarprajapati42982@gmail.com>
2026-08-27 21:33:48 +0530

    Time: 8 ms (71.86%), Space: 94.1 MB (86.24%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    int SolveTab(vector<int>& nums1, vector<int>& nums2){
+    int SolveSpace(vector<int>& nums1, vector<int>& nums2){
         int n=nums1.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        int swap=0;
+        int noswap=0;
+        int currswap=0;
+        int currnoswap=0;
+
         for(int index=n-1;index>=1;index--){
             for(int swapped=1;swapped>=0;swapped--){
                 int ans=INT_MAX;
                 int prev1=nums1[index-1];
                 int prev2=nums2[index-1];
-                if(swapped) swap(prev1,prev2);
+                if(swapped) {
+                    int temp=prev2;
+                    prev2=prev1;
+                    prev1=temp;
+                }
+
                 if(nums1[index]>prev1 && nums2[index]>prev2)
-                  ans=dp[index+1][0];
+                  ans=noswap;
                 if(nums1[index]>prev2 && nums2[index]>prev1)
-                   ans=min(ans,1+dp[index+1][1]);
-                dp[index][swapped]=ans;      
+                   ans=min(ans,1+swap);
+                if(swapped) currswap = ans;
+                else currnoswap=ans;        
             }
+            swap=currswap;
+            noswap=currnoswap;
         }
-        return  dp[1][0];    
+        return min(swap,noswap);    
     }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(),-1);
         nums2.insert(nums2.begin(),-1);
         bool swapped=0;
         int n=nums1.size();
-        return SolveTab(nums1,nums2);
+        return SolveSpace(nums1,nums2);
         
     }
 };
