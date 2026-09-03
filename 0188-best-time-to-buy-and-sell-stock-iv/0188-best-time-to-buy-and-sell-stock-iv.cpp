9c24f390c538a7208fba12ad8e48737d3ca3b48b
Amar Prajapati <amarprajapati42982@gmail.com>
2026-09-03 16:13:49 +0530

    Time: 11 ms (44.63%), Space: 17.8 MB (58.88%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-  int Solve(vector<int>& prices,int k){
-        int n = prices.size();
-        vector<vector<int>> curr(2,vector<int>(k+1,0));
-        vector<vector<int>> next(2,vector<int>(k+1,0));
-        for(int index=n-1;index>=0;index--){
-            for(int buy=0;buy<=1;buy++){ 
-                for(int limit=1;limit<=k;limit++){
-                    int profit=0;
-                    if(buy){
-                       int buyKaro=-prices[index]+next[0][limit];
-                       int skipKaro=0+next[1][limit];
-                        profit=max(buyKaro,skipKaro);
-                  }else{
-                       int sellKaro=prices[index]+next[1][limit-1];
-                      int skipKaro=0+next[0][limit];
-                      profit=max(sellKaro,skipKaro);
-                    }
-                    curr[buy][limit]=profit;
-                }
-            }
-            next = curr;
+  int SolveMem(int index,int operationNo,int k,vector<int>& prices,vector<vector<int>>&dp){
+     if(index==prices.size()) return 0;
+        if(operationNo==2*k)  return 0;
+        if(dp[index][operationNo]!=-1) return dp[index][operationNo];
+        int profit=0;
+        if(operationNo%2==0){
+            int buyKaro=-prices[index]+SolveMem(index+1,operationNo+1,k,prices,dp);
+            int skipKaro=0+SolveMem(index+1,operationNo,k,prices,dp);
+            profit=max(buyKaro,skipKaro);
+        }else{
+            int sellKaro=prices[index]+SolveMem(index+1,operationNo+1,k,prices,dp);
+            int skipKaro=0+SolveMem(index+1,operationNo,k,prices,dp);
+            profit=max(sellKaro,skipKaro);
         }
-        return next[1][k];
-    }       
+        return dp[index][operationNo]=profit;
+    }
+    
     int maxProfit(int k,vector<int>& prices) {
-        return Solve(prices,k);  
+        int n = prices.size();
+        vector<vector<int>>dp(n,vector<int>(2*k,-1));
+        return SolveMem(0,0,k,prices,dp);  
     }
 };
