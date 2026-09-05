1dcd779ef4756bafca0112cbd6ae2752250e9382
Amar Prajapati <amarprajapati42982@gmail.com>
2026-09-05 09:20:19 +0530

    Time: 5 ms (73.33%), Space: 10.5 MB (93.64%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int SolveTab(string& a,string& b){
-        vector<vector<int>>dp(a.length()+1,vector<int>(b.length()+1,0));
+    int SolveSpace(string& a,string& b){
+        vector<int>curr(b.length()+1,0);
+        vector<int>next(b.length()+1,0);
         for(int j=0;j<b.length();j++){
-            dp[a.length()][j]=b.length()-j;
-        }
-        for(int i=0;i<a.length();i++){
-            dp[i][b.length()]=a.length()-i;
+            next[j]=b.length()-j; 
         }
         for(int i=a.length()-1;i>=0;i--){
             for(int j=b.length()-1;j>=0;j--){
+                curr[b.length()]=a.length()-i;
                 int ans=0;
                 if(a[i]==b[j]){
-                ans= dp[i+1][j+1];
+                ans= next[j+1];
                 }else{
-                    int insertAns=1+dp[i][j+1];
-                    int deleteAns=1+dp[i+1][j];
-                    int replaceAns=1+dp[i+1][j+1];
+                    int insertAns=1+curr[j+1];
+                    int deleteAns=1+next[j];
+                    int replaceAns=1+next[j+1];
                     ans=min(insertAns,min(deleteAns,replaceAns));
                 }
-                dp[i][j]=ans;
+                curr[j]=ans;
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
         }
     int minDistance(string word1, string word2) {
-        return SolveTab(word1,word2); 
+        if(word1.length()==0)  return word2.length();
+        if(word2.length()==0) return word1.length();
+        return SolveSpace(word1,word2); 
     }
 };
