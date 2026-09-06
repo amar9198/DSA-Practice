44470d138898c7aa230a1901220c40ec81210264
Amar Prajapati <amarprajapati42982@gmail.com>
2026-09-06 12:02:51 +0530

    Time: 92 ms (18.27%), Space: 98.5 MB (6.13%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    bool SolveMem(string& str,string& pattern,int i, int j,vector<vector<int>>& dp){
-        if(i==0 && j==0) return true;
-        if(i>0 && j==0)  return false;
-        if(i==0 && j>0){
+    bool SolveTab(string& str,string& pattern){
+        vector<vector<int>>dp(str.length()+1,vector<int>(pattern.length()+1,0));
+        dp[0][0]=true;
+        for(int j=1;j<=pattern.length();j++){
+            bool flag=true;
             for(int k=1;k<=j;k++){
                 if(pattern[k-1]!='*'){
-                    return false;
+                    flag=false;
+                    break;
                 }
             } 
-            return true;
+            dp[0][j]=flag;
         }
-    if(dp[i][j]!=-1) 
-        return dp[i][j];
-    if(str[i-1]==pattern[j-1] || pattern[j-1] == '?')
-       return dp[i][j]=SolveMem(str,pattern,i-1,j-1,dp);
-    else if(pattern[j-1]=='*')
-        return dp[i][j] = (SolveMem(str,pattern,i-1,j,dp)||SolveMem(str,pattern,i,j-1,dp));
-    else  return false;    
-    }   
+        for(int i=1;i<=str.length();i++){
+            for(int j=1;j<=pattern.length();j++){
+                if(str[i-1]==pattern[j-1] || pattern[j-1] == '?')
+                  dp[i][j]=dp[i-1][j-1];
+                else if(pattern[j-1]=='*')
+                   dp[i][j] = dp[i-1][j]||dp[i][j-1];
+                else  dp[i][j]= false;    
+            }   
+        }
+        return dp[str.length()][pattern.length()];
+
+    }
+  
     bool isMatch(string s, string p) {
         vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
-        return SolveMem(s,p,s.length(),p.length(),dp);  
+        return SolveTab(s,p);  
     }
 };
